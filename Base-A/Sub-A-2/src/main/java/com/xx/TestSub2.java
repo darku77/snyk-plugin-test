96b573e32dc7d2d92ce1bec847f1d3c851b12657
darku77 <dark_raven2311@yahoo.com>
2022-04-28 18:26:46 +0300

    snyk plugin

---
+++ Base-A/Sub-A-2/src/main/java/com/xx/TestSub2.java
@@ -0,0 +1,16 @@
+// =============================================================================
+// Copyright/Security Notice
+//
+// Licensed Materials - Property of Qnamic AG
+// (C) Copyright Qnamic AG 2003-2022
+// All rights reserved
+//
+// End Copyright
+// =============================================================================
+package com.xx;
+
+/**
+ * @author Catalin on 28.04.2022
+ */
+public class TestSub2 {
+}
